d954810e9e0bf2a01f167f6c578a625f89c6d3a9
Pedro Elias <epedro520@gmail.com>
2020-08-07 00:12:16 -0300

    Search metod as been created

--- src/Models/Biblioteca.kt
+++ src/Models/Biblioteca.kt
@@ -1,14 +1,22 @@
 package Models
 
-
 class Biblioteca () {
     companion object {
         var list =  mutableListOf<Livro>()
         fun adicionarLivro() {
             val titulo = readLine()
             val ano = readLine()
+            list.add(Livro(titulo,ano))
         }
         fun pesquisarPorAutor() {
+            print("Digite o nome do Autor que quer pesquisar!")
+            val nome_pesquisa = readLine()
+            for (item in list.length) {
+                val nome_pesquisa = list[item].nome
+                if (nome == nome_pesquisa) {
+                    print("O Autor está na list,na posição $item")
+                }
+            }
 
         }
         fun pesquisarPorTitulo() {
