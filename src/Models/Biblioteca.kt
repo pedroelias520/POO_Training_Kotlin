25bb4b6acecdfcbe12d61092f5ef897aaee37a1a
Pedro Elias <epedro520@gmail.com>
2020-08-10 07:53:36 -0300

    "Test stage , bug fixes"

--- src/Models/Biblioteca.kt
+++ src/Models/Biblioteca.kt
@@ -11,20 +11,30 @@ class Biblioteca () {
             list.add(Livro(titulo,ano.toInt()))
         }
         fun pesquisarPorAutor() {
-            print("Digite o nome do Autor que quer pesquisar!")
-            val autor_pesquisa = readLine().toString()        
+            print("Digite o nome do Autor que quer pesquisar! \n")
+            val autor_pesquisa = readLine().toString()     
+            var autor_encontrado: Boolean = false   
+            var livros_encontrados = mutableListOf<String>()
             val list_size = list.size   
             for (item in list) {
                 val nome_pesquisa = item.autores
                 for (nomes_autores in nome_pesquisa){
-                    if (autor_pesquisa.equals(nomes_autores.primeiroNome+nomes_autores.ultimoNome)){
-                        print("Autor encontrado! O autor se enconta na posicao: "+item)
-                    }
-                    else{
-                        print("Este autor não foi encontrado \n talvez você não o tenha adicionado a lista de autores")
-                    }
+                    if (autor_pesquisa.equals(nomes_autores.primeiroNome+" "+nomes_autores.ultimoNome)){
+                        autor_encontrado = true                        
+                        livros_encontrados.add(item.titulo)
+                    }                                      
                 }
             }
+            if(autor_encontrado){
+                print("Autor encontrado!")
+                print("Livros que ele aparece : \n")
+                for(i in livros_encontrados){
+                    print(i)
+                }
+            }
+            else {
+                print("Ah não :/ ... o seu autor não foi encontrado!")
+            }            
 
         }
         fun pesquisarPorTitulo() {
--- src/main.kt
+++ src/main.kt
@@ -4,16 +4,17 @@ package com.bezkoder.kotlin.numberparser
 fun main(args: Array<String>) {
     var operation_flow: Boolean = false 
     var operation: String
-    var biblio = Biblioteca()
+    val biblio: Biblioteca = Biblioteca()
     while (operation_flow == true){
         print("Bem-vindo \n QUAL OPERAÇÃO DESEJA FAZER? \n")
         print(" 1- Cadastrar livro \n 2 - Pesquisar por Livro \n 3 - Pesquisar por Autor \n 4 - Adicionar Livro")
         operation = readLine().toString()
 
-        when (operation){
-            is 1 -> biblio.adicionarLivro()
-            is 2 -> biblio.pesquisarPorTitulo()
-            is 3 -> biblio.pesquisarPorAutor()            
+        when (operation.toInt()){
+            1 -> biblio.adicionarLivro()
+            2 -> biblio.pesquisarPorTitulo()
+            3 -> biblio.pesquisarPorAutor() 
+            else ->  "Obrigado por usar nosso programa - keep_society.bat"       
         }
 
         
@@ -24,7 +25,7 @@ fun main(args: Array<String>) {
         val nome_livro = readLine().toString()
         print("Digite o ano que foi escrito")
         val ano_livro = readLine().toString()
-        val livro = Livro(nome_livro, ano_livro.toInt())
+        val livro: Livro = Livro(nome_livro, ano_livro.toInt())
         print("Deseja Adicionar um autor? \n 1 - SIM \n 2 -NÃO")
         var y_n:String = readLine().toString()
         while(y_n.toInt() == 1) {
