bee4b2c6306a554652b2b08f27d3e11e86aeaa92
Pedro Elias <epedro520@gmail.com>
2020-08-09 21:36:20 -0300

    List of Autores created

--- src/Models/Biblioteca.kt
+++ src/Models/Biblioteca.kt
@@ -1,26 +1,28 @@
 package Models
+package com.bezkoder.kotlin.numberparser
 
 class Biblioteca () {
     companion object {
         var list =  mutableListOf<Livro>()
         fun adicionarLivro() {
-            val titulo = readLine()
-            val ano = readLine()
-            list.add(Livro(titulo,ano))
+            val titulo: String = readLine().toString()
+            val ano:String = readLine().toString()
+            list.add(Livro(titulo,ano.toInt()))
         }
         fun pesquisarPorAutor() {
             print("Digite o nome do Autor que quer pesquisar!")
-            val nome_pesquisa = readLine()
-            for (item in list.length) {
-                val nome_pesquisa = list[item].nome
-                if (nome == nome_pesquisa) {
-                    print("O Autor está na list,na posição $item")
+            val nome_pesquisa = readLine()        
+            val list_size = list.size   
+            for (item in list) {
+                val nome_pesquisa = item.autores
+                for (nomes_autores in nome_pesquisa){
+
                 }
             }
 
         }
         fun pesquisarPorTitulo() {
-            
+
         }
     }
 
--- src/Models/Livro.kt
+++ src/Models/Livro.kt
@@ -1,14 +1,12 @@
 package Models
 
 class Livro constructor(val titulo:String, val ano: Int) {
-       val autores = mutableListOf<Autor>()
+       var autores = mutableListOf<Autor>()
        fun AdicionarAutor() {
            val autor_nome = readLine().toString()           
            val autor_2nome = readLine().toString()
            val autor_email = readLine().toString()
-
-           autores.add(Autor(autor_nome, autor_2nome, autor_email)) 
-           
+           autores.add(Autor(autor_nome, autor_2nome, autor_email))            
        }
        fun ToString(){
            print("")
