eb79cb2fcfdb8de65a06d06cc2461cbfc7d1f0ae
Pedro Elias <epedro520@gmail.com>
2020-08-10 08:04:22 -0300

    solving "unresolved reference" in classes

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,11 @@
 package Models
 package com.bezkoder.kotlin.numberparser
 
+
 fun main(args: Array<String>) {
     var operation_flow: Boolean = false 
     var operation: String
-    val biblio: Biblioteca = Biblioteca()
+    val biblio = Biblioteca()
     while (operation_flow == true){
         print("Bem-vindo \n QUAL OPERAÇÃO DESEJA FAZER? \n")
         print(" 1- Cadastrar livro \n 2 - Pesquisar por Livro \n 3 - Pesquisar por Autor \n 4 - Adicionar Livro")
@@ -25,7 +26,7 @@ fun main(args: Array<String>) {
         val nome_livro = readLine().toString()
         print("Digite o ano que foi escrito")
         val ano_livro = readLine().toString()
-        val livro: Livro = Livro(nome_livro, ano_livro.toInt())
+        val livro = Livro(nome_livro, ano_livro.toInt())
         print("Deseja Adicionar um autor? \n 1 - SIM \n 2 -NÃO")
         var y_n:String = readLine().toString()
         while(y_n.toInt() == 1) {
@@ -35,6 +36,7 @@ fun main(args: Array<String>) {
         }        
         print("Livro adicionado!")
     }
+
    
 
 }
